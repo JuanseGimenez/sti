48d0fff93f1a8a9c1c0d29c9410bcc68315e0796
Juanse Gimenez <juanse@sinequix.com>
2017-12-28 12:10:53 -0300

    Created models empty and added scopes

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,2 +1,16 @@
 class Animal < ApplicationRecord
+	belongs_to :tribe
+	self.inheritance_column = :race
+
+	validates :race, presence: true
+
+	scope :lions, -> { where(race: 'Lion') }
+	scope :meerkats, -> { where(race: 'Meerkat') }
+	scope :wild_boars, -> { where(race: 'WildBoar') }
+
+	# We will need a way to know which animals
+  # will subclass the Animal model
+  def self.races
+  	%w(Lion WildBoar Meerkat)
+  end
 end
